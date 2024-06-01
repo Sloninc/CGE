d72017802847db86896949236bfd8d0e23e40e9b
Andrei Ruk <sloninc@mail.ru>
2024-06-01 12:44:28 +0300

    Добавлен проект второго задания.

---
+++ CGE_2/Cat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGE_2
+{
+    public abstract class Cat
+    {
+        public int age = 4;
+        public string name;
+        public int tailCount = 1;
+    }
+}
---
+++ CGE_2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection;
+using System.Reflection.Emit;
+
+namespace CGE_2
+{
+    public delegate Cat CatCreator(int i, string s, int t);
+    internal class Program
+    {
+        static void Main()
+        {
+            Type type = Type.GetType("CGE_2.Cat");
+             var f = type.GetFields();
+
+            var assembly = new AssemblyName("DynamicCGE_2");
+            AssemblyBuilder ab = AssemblyBuilder.DefineDynamicAssembly(assembly, AssemblyBuilderAccess.Run);
+
+            ModuleBuilder mb = ab.DefineDynamicModule(assembly.Name);
+
+            TypeBuilder tb = mb.DefineType("InstCat", TypeAttributes.Public, type);
+
+            Type[] parameterTypes = { typeof(int), typeof(string), typeof(int) };
+
+            ConstructorBuilder ctor = tb.DefineConstructor(MethodAttributes.Public, CallingConventions.Standard, parameterTypes);
+            ILGenerator ctorIL = ctor.GetILGenerator();
+
+            ctorIL.Emit(OpCodes.Ldarg_0);
+            ctorIL.Emit(OpCodes.Ldarg_1);
+            ctorIL.Emit(OpCodes.Stfld, f[0]);
+            ctorIL.Emit(OpCodes.Ldarg_0);
+            ctorIL.Emit(OpCodes.Ldarg_2);
+            ctorIL.Emit(OpCodes.Stfld, f[1]);
+            ctorIL.Emit(OpCodes.Ldarg_0);
+            ctorIL.Emit(OpCodes.Ldarg_3);
+            ctorIL.Emit(OpCodes.Stfld, f[2]);
+            ctorIL.Emit(OpCodes.Ret);
+            Type? t = tb.CreateType();
+
+            Cat cat = null;
+            if (t != null)
+                cat = (Cat)Activator.CreateInstance(t, new object[] { 1, "Tom", 2 });
+            Console.WriteLine(cat.age + " " + cat.name + " " + cat.tailCount + " " + cat.GetType().BaseType);
+            Console.ReadLine();
+        }
+    }
+}
